62b683a3f6beb89c9b78981902b149dbf13476c1
dillon <dhirth.it@gmail.com>
2021-09-20 16:58:02 -0500

    Syntax fixes and added $packageName

--- CICD/build.ps1
+++ CICD/build.ps1
@@ -1,15 +1,16 @@
 ﻿Set-Location -Path C:\git\DiscordBots\LinkFixer
 $computerName = "COWSUNRAID"
 $userId = "root"
-$pwd = Get-Content "$PSScriptRoot\_creds.txt" | ConvertTo-SecureString
-$creds = New-Object System.Management.Automation.PSCredential -ArgumentList $userId, $pwd
+$pass = Get-Content ".\_creds.txt" | ConvertTo-SecureString -AsPlainText -Force
+$creds = New-Object System.Management.Automation.PSCredential -ArgumentList $userId, $pass
+$packageName = "linkFixerImage_1_0_4.tar"
 
 
 
-py -m pip freeze > requirements.txt
-docker build --tag link-fixer:v1.0.4 .
-docker save -o .\linkFixerImage_v1_0_4.tar link-fixer:v1.0.4
-Copy-Item -Path c:\git\DiscordBots\linkFixerImage_v1_0_1.tar -Destination \\COWSUNRAID\Files\DiscordBots\LinkFixer
+py -m pip freeze > $PWD\requirements.txt
+docker build --tag link-fixer:1.0.4 .
+docker save -o ..\$packageName link-fixer:1.0.4
+Copy-Item -Path c:\git\DiscordBots\$packageName -Destination \\COWSUNRAID\Files\DiscordBots\LinkFixer
 
 
 
@@ -18,7 +19,7 @@ try {
     $sshSession = New-SSHSession -ComputerName $computerName -Credential $creds -AcceptKey -ConnectionTimeout 10 -ErrorAction Stop
     $sessionId = $sshSession.SessionId
     "Session $sessionId opened."
-    $command = "docker load -i /mnt/user/Files/linkFixerImage_v1_0_4.tar"
+    $command = "docker load -i /mnt/user/Files/DiscordBots/LinkFixer/$packageName"
     "Command: {0}" -f $command
     $sshOut = (Invoke-SSHCommand -SessionId $sessionId -Command $command).Output
     "Results: '{0}'" -f $sshOut
